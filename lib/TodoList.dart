6e48b42f69072e9c21ee6770a49c27c1ee640bf1
sovath.koeun <sovath.koeun@pnc.lan>
2020-01-26 11:14:32 +0700

    projects_todolist

---
+++ lib/TodoList.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'Todo.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  List<Todo> todos = [];
+  TextEditingController controller = new TextEditingController();
+    _toggleTodo(Todo todo, bool isChecked) {
+        setState(() {
+        todo.isDone = isChecked;
+    });
+   }
+
+    Widget _buildItem(BuildContext context, int index) {
+    final todo = todos[index];
+    return CheckboxListTile(
+      value: todo.isDone,
+      title: Text(todo.title),
+       onChanged: (bool isChecked) {
+       _toggleTodo(todo, isChecked);
+      },
+     );
+   }
+  _addTodo()  {
+    showDialog<Todo>(
+       context: context,
+       builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0)
+          ),
+           title: Text('New todo'),
+           content: TextField(
+           controller: controller,
+           autofocus: true,
+           ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancel"),
+               onPressed: () {
+               Navigator.of(context).pop();
+              },    
+            ),
+
+            FlatButton(
+              child: Text("Add", style: TextStyle(color: Colors.blue),),
+              onPressed: () {
+              setState(() {
+              final todo = new Todo(title: controller.value.text);
+              if(controller.value.text == ""){
+                print("");
+              }else{
+                todos.add(todo);
+              }
+              controller.clear();
+              Navigator.of(context).pop();
+                });
+              },
+            ),
+          ],
+        );
+       },
+     );
+   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(title: Text('Todo List')),
+       body: ListView.builder(
+           itemBuilder: _buildItem,
+        itemCount: todos.length,
+       ),
+       // button add for checbox
+       floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+         onPressed: _addTodo,
+       ),
+    );
+  }
+}
